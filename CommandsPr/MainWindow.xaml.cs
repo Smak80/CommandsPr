cb42e376c1e71bffebd762c7b000e4831f888228
Сергей Владиславович Маклецов <smak.80@yandex.ru>
2025-03-26 14:53:18 +0300

    Добавьте файлы проекта.

---
+++ CommandsPr/MainWindow.xaml.cs
@@ -0,0 +1,56 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace CommandsPr
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private Random random = new Random();
+        private int _currentButton;
+        public MainWindow()
+        {
+            InitializeComponent();
+            _currentButton = random.Next(3); 
+        }
+
+        private void Sit_Executed(object sender, ExecutedRoutedEventArgs e)
+        {
+            MessageBox.Show("Сижу");
+            _currentButton = random.Next(3);
+        }
+        private void Lay_Executed(object sender, ExecutedRoutedEventArgs e)
+        {
+            MessageBox.Show("Лежу");
+            _currentButton = random.Next(3);
+        }
+        private void Voice_Executed(object sender, ExecutedRoutedEventArgs e)
+        {
+            MessageBox.Show("Гав-гав");
+            _currentButton = random.Next(3);
+        }
+        
+        private void Sit_CanExecute(object sender, CanExecuteRoutedEventArgs e)
+        {
+            e.CanExecute = _currentButton == 0;
+        }
+        private void Lay_CanExecute(object sender, CanExecuteRoutedEventArgs e)
+        {
+            e.CanExecute = _currentButton == 1;
+        }
+        private void Voice_CanExecute(object sender, CanExecuteRoutedEventArgs e)
+        {
+            e.CanExecute = _currentButton == 2;
+        }
+    }
+}
---
+++ CommandsPr/MyCoomands.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace CommandsPr
+{
+    class MyCoomands
+    {
+        public static RoutedCommand Sit { get; set; }
+        public static RoutedCommand Lay { get; set; }
+        public static RoutedCommand Voice { get; set; }
+        static MyCoomands() 
+        {
+            Sit = new RoutedCommand(nameof(Sit), typeof(MainWindow));
+            Lay = new RoutedCommand(nameof(Lay), typeof(MainWindow));
+            Voice = new RoutedCommand(nameof(Voice), typeof(MainWindow));
+        }
+    }
+}
